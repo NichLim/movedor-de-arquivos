712c0439a6b77d217c775c2052b0527e1df7286f
NichLim <nicholaspointcom@gmail.com>
2025-04-11 11:02:56 -0300

    Updated gitignore with all .toml files Added Ruff formatter Updated %s format strings to f strings Used ruff to format the code

--- app.py
+++ app.py
@@ -5,57 +5,80 @@ import sys
 import time
 import logging
 
-timestr = time.strftime('%d.%m.%Y %H.%M.%S')
-logger = logging.getLogger("main") # inicializa o arquivo de Log
-#configura o logger e a formatação da mensagem para inclusão de hora/data nas mensagens
-logging.basicConfig(filename = "Log - %s.log" %timestr, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S') 
+timestr = time.strftime("%d.%m.%Y %H.%M.%S")
+logger = logging.getLogger("main")  # inicializa o arquivo de Log
+# configura o logger e a formatação da mensagem para inclusão de hora/data nas mensagens
+logging.basicConfig(
+    filename=f"Log - {timestr}.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S",
+)
 
-logger.info('Inicializando')
+logger.info("Inicializando")
 
-with open('config.json') as config: #extraindo as informações de whitelist, origem e destino do arquivo config.json da pasta do programa
+with (
+    open("config.json") as config
+):  # extraindo as informações de whitelist, origem e destino do arquivo config.json da pasta do programa
     config_from_JSON = json.load(config)
-    logger.info('Carregando informações do JSON')
-    
-    logger.info('Validando Pasta de Origem')
-    if os.path.exists(config_from_JSON["origem"]):  #Verifica se o endereço original dentro do JSON é um caminho válido no Sistema
+    logger.info("Carregando informações do JSON")
+
+    logger.info("Validando Pasta de Origem")
+    if os.path.exists(
+        config_from_JSON["origem"]
+    ):  # Verifica se o endereço original dentro do JSON é um caminho válido no Sistema
         origem = config_from_JSON["origem"]
-        logger.info('Endereço de Origem %s válido', origem)
+        logger.info(f"Endereço de Origem {origem} válido")
     else:
-        logger.warning('Origem Inválida. Por favor, verifique o arquivo config.json')
-        logger.error('Um erro foi encontrado durante a execução do programa, por favor verifique o Log')
-        sys.exit(0)
-    
-    logger.info('Validando Pasta de Destino')
-    if os.path.exists(config_from_JSON["destino"]):  #Verifica se o endereço de destino dentro do JSON é um caminho válido no Sistema
-        destino = config_from_JSON["destino"]
-        logger.info('Endereço de Destino %s válido', destino)
-    else:
-        logger.warning('Destino Inválido. Por favor, verifique o arquivo config.json')
-        logger.error('Um erro foi encontrado durante a execução do programa, por favor verifique o Log')
+        logger.warning("Origem Inválida. Por favor, verifique o arquivo config.json")
+        logger.error(
+            "Um erro foi encontrado durante a execução do programa, por favor verifique o Log"
+        )
         sys.exit(0)
 
-    logger.info('Verificando Whitelist')
-    if isinstance(config_from_JSON["whitelist"], list): #Verifica se a Whitelist é uma Lista Python válida (arrays são resolvidos como Listas quando são importados)
-        whitelist = config_from_JSON["whitelist"]
-        if not whitelist: #Checa se a Whitelist está vazia
-            logger.warning('Whitelist Vazia, isto transferirá todos os arquivos da pasta de origem')
-        else:
-            logger.info('Whitelist Válida: %s', whitelist)
+    logger.info("Validando Pasta de Destino")
+    if os.path.exists(config_from_JSON["destino"]):  
+        # Verifica se o endereço de destino dentro do JSON é um caminho válido no Sistema
+        destino = config_from_JSON["destino"]
+        logger.info(f"Endereço de Destino {destino} válido")
     else:
-        logger.warning('Whitelist Inválida. Por favor verifique o arquivo config.json')
-        logger.error('Um erro foi encontrado durante a execução do programa, por favor verifique o Log')
+        logger.warning("Destino Inválido. Por favor, verifique o arquivo config.json")
+        logger.error(
+            "Um erro foi encontrado durante a execução do programa, por favor verifique o Log"
+        )
+        sys.exit(0)
+
+    logger.info("Verificando Whitelist")
+    if isinstance(config_from_JSON["whitelist"], list):  
+        # Verifica se a Whitelist é uma Lista Python válida (arrays são resolvidos como Listas quando são importados)
+        whitelist = config_from_JSON["whitelist"]
+        if not whitelist:  # Checa se a Whitelist está vazia
+            logger.warning(
+                "Whitelist Vazia, isto transferirá todos os arquivos da pasta de origem"
+            )
+        else:
+            logger.info(f"{whitelist = }")
+    else:
+        logger.warning("Whitelist Inválida. Por favor verifique o arquivo config.json")
+        logger.error(
+            "Um erro foi encontrado durante a execução do programa, por favor verifique o Log"
+        )
         sys.exit(0)
     config.close()
 
 
-arquivos = list(set(os.listdir(origem)) - set(whitelist)) #lista com os arquivos da pasta sem os arquivos da whitelist
+arquivos = list(
+    set(os.listdir(origem)) - set(whitelist)
+)  # lista com os arquivos da pasta sem os arquivos da whitelist
 
 for arquivo in arquivos:
-    if not arquivos or (len(arquivos) == 1 and os.path.join(origem, arquivo) == destino):  #Checa se há algo para transferir e, caso só haja o arquivo de destino para transferir, emite erro e encerra o programa
-        logger.warning('Não há arquivos a serem transferidos')
+    if (
+        not arquivos
+        or (len(arquivos) == 1 and os.path.join(origem, arquivo) == destino)
+    ):  # Checa se há algo para transferir e, caso só haja o arquivo de destino para transferir, emite erro e encerra o programa
+        logger.warning("Não há arquivos a serem transferidos")
         sys.exit(0)
-    if os.path.join(origem, arquivo) == destino: #Prevenção de Transferencia de arquivos para dentro deles mesmos
-        continue
     shutil.move(os.path.join(origem, arquivo), os.path.join(destino, arquivo))
-    logger.info('Transferindo %s', arquivo)
-    logger.info('Transferencia Completa.')
+    logger.info(f"Transferindo {arquivo}")
+    logger.info("Transferencia Completa.")
